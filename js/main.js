e5889fdc25b589da6fdc3ec860c3b9e656642836
aleks <avz1984@gmail.com>
2021-03-24 15:41:31 +0300

    First JS software

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-let number = 5;
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt("Один из просмотренных фильмов?", "");
+const b = prompt("На сколько бы вы оценили его?", "");
+const c = prompt("Один из просмотренных фильмов?", "");
+const d = prompt("На сколько бы вы оценили его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
