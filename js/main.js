b659847be45d0539bf8e514dfd83a3d3f41012e8
aleks <avz1984@gmail.com>
2021-04-15 09:24:46 +0300

    homework

--- js/main.js
+++ js/main.js
@@ -8,12 +8,26 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt("Один из просмотренных фильмов?", "");
-const b = prompt("На сколько бы вы оценили его?", "");
-const c = prompt("Один из просмотренных фильмов?", "");
-const d = prompt("На сколько бы вы оценили его?", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
 console.log(personalMovieDB);
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из просмотренных фильмов?", "");
+  const b = prompt("На сколько бы вы оценили его?", "");
+
+  if (a !== null && b !== null && a !== "" && b !=="" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--; 
+  }
+  personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 || personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else {
+  console.log("Вы киноман");
+}
