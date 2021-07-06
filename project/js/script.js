5cc145565ea35c0bed6354b6adde33bbe4b3a176
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-07-06 17:17:25 +0300

    Homework

--- project/js/script.js
+++ project/js/script.js
@@ -14,7 +14,22 @@
 
 'use strict';
 
-const movieDB = {
+//const newElement = document.createElement("li");
+//newElement.classList.add("promo__interactive-item");
+//newElement.textContent = movieDB.movies[2];
+//moviesList.prepend(newElement);
+
+
+//i в вышеуказанном случае это нумерация, + 1 для того. чтобы список начинался с 1, а не с 0, так как в массивах идет нумерация с 0;
+
+//a = a + 1;
+//a += 1;  //тоже самое, что и верх
+
+//a = a + "aaa";
+//a += "aaa"; //тоже самое, что и верх
+
+document.addEventListener("DOMContentLoaded",() =>  {
+  const movieDB = {
     movies: [
         "Логан",
         "Лига справедливости",
@@ -28,55 +43,70 @@ const promoAdvertisement = document.querySelectorAll(".promo__adv img");
 const promoGenre = document.querySelector(".promo__genre");
 const promoBackground = document.querySelector(".promo__bg");
 const moviesList = document.querySelector(".promo__interactive-list");
+const form = document.querySelector(".add");
+const input = form.querySelector(".adding__input");
+const checkbox = form.querySelector("[type='checkbox']");
+const MAX__NUMBERS = 21;
 
-promoAdvertisement.forEach((item) => {
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  let newFilm = input.value;
+  const favorite = checkbox.checked;
+  
+  if (newFilm) {
+    if (newFilm.length > MAX__NUMBERS) {
+      newFilm = `${newFilm.substring(0, 22)}...`;
+    }
+    if (favorite) {
+      console.log("Ничего не понимаю, сам сделать не смог ничего, все по уроку");
+    }
+    movieDB.movies.push(newFilm);
+    mainSort(movieDB.movies); 
+    createMovieList(movieDB.movies, moviesList);
+  }
+  
+  form.reset(); 
+});
+
+const deleteAds = (array) => {
+  array.forEach((item) => {
     item.remove();
 });
-promoGenre.textContent = "Драма";
-promoBackground.style.cssText = "background-image: url(img/bg.jpg)";
+};
+deleteAds(promoAdvertisement);
 
-moviesList.innerHTML = "";
-movieDB.movies.sort();
+const makingChanges = (genre, backgroundPicture) => {
+  genre.textContent = "Драма";
+  backgroundPicture.style.cssText = "background-image: url(img/bg.jpg)";
+};
+makingChanges(promoGenre, promoBackground);
 
-//const newElement = document.createElement("li");
-//newElement.classList.add("promo__interactive-item");
-//newElement.textContent = movieDB.movies[2];
-//moviesList.prepend(newElement);
 
-movieDB.movies.forEach((item, i) => {
-    moviesList.innerHTML += `<li class="promo__interactive-item">${i+1 + "."} ${item}
+const mainSort = (array) => {
+  array.sort();
+};
+
+
+
+const createMovieList = (films, parent) => {
+  parent.innerHTML = "";
+  mainSort(movieDB.movies);
+  films.forEach((item, i) => {
+  parent.innerHTML += `<li class="promo__interactive-item">${i+1 + "."} ${item}
     <div class="delete"></div>
   </li>`;
 });
-
-const addMovie = () => {
-movieDB.movies.forEach((item, i) => {
-  moviesList.innerHTML += `<li class="promo__interactive-item">${i+1 + "."} ${item}
-  <div class="delete"></div>
-</li>`;
-});
+document.querySelectorAll(".delete").forEach((item, i) => {
+  item.addEventListener("click", () => {
+    item.parentElement.remove();
+    movieDB.movies.splice(i, 1);
+    createMovieList(films, parent);
+  });
+}); 
 };
 
-//i в вышеуказанном случае это нумерация, + 1 для того. чтобы список начинался с 1, а не с 0, так как в массивах идет нумерация с 0;
+createMovieList(movieDB.movies, moviesList);
 
-//a = a + 1;
-//a += 1;  //тоже самое, что и верх
-
-//a = a + "aaa";
-//a += "aaa"; //тоже самое, что и верх
-
-const addingInput = document.querySelector(".adding__input");
-const addButton = document.querySelector("button");
-const MAX__NUMBERS = 21;
-
-
-
-addButton.addEventListener("click", (evt) => {
-   if(addingInput !== "") {
-    evt.preventDefault();
-    movieDB.movies.push(addingInput.value);
-    console.log(movieDB.movies)
-    addMovie();
-    addingInput.value = "";
-   }
 });
+
+ 
