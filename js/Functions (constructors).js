0555160257965632e79fdb4c8adc93f2af2eb457
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-07-28 18:05:36 +0300

    classes and before them ES5

---
+++ js/Functions (constructors).js
@@ -0,0 +1,50 @@
+'use strict'
+
+const num = new Number(3);  //создание нового элемента, старое, не используется
+console.log(num)
+
+const numNew = new Function(3); // старое, не используется
+
+// Стандарт ES5 (сейчас такой функционал создается с помощью классов, которых изначально не было)
+
+const user = function(name, id) {
+  this.name = name;     // this не работает в стрелочных функциях
+  this.id = id;
+  this.human = true;
+  this.hello = function () {
+    console.log(`Hello ${this.name}`);
+  };
+};
+
+user.prototype.exit = function( ) {
+  console.log(`user ${this.name} left`);
+};
+
+const ivan = new user("Ivan", 28);
+const alex = new user("Alex", 29);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
+
+// конструкторы необходимы для создания новых однотипных объектах
+
+// пример создания через классы
+
+class userNew {
+  constructor(name, id) {
+    this.name = name;     
+    this.id = id;
+    this.human = true;
+  }
+  hello() {
+    console.log(`Hello ${this.name}`);
+  }
+  exit() {
+    console.log(`user ${this.name} left`);
+  }
+}
