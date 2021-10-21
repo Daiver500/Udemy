442ffed4d98d121ff6372877562c04029c612e11
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-10-21 15:02:57 +0300

    fetch api

--- Main project/dist/js/script(fetchAPI).js
+++ Main project/dist/js/script(fetchAPI).js
@@ -304,17 +304,5 @@ class MenuCard {
       thanksModal.remove();
     }
     modalClose.addEventListener("click", closeThanksModal);
-  }
-
-  // API // набор данных и возможностей, который предоставляет нам какое-то готовое решение
-  // FETCH API
-  fetch("https://jsonplaceholder.typicode.com/posts", {   // в скобки помещается тот url на который мы будем делать GET или POST запросы, из данной конструкции нам возвращается Promise
-    method: "POST",                                         // отправляем данные
-    body: JSON.stringify({name: "Alex"}),           // отправим объект с помощью FETCH
-    headers: {
-      "Content-type": "application/json"
-    }                   
-  })      
-  .then(response => response.json())        // здесь получаем ответ в json формате и далее трансформируем его в обычный объект с помощью response.json, здесь возвращается Promise
-  .then(json => console.log(json))           
+  }      
  });
