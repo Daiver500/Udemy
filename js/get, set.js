35bb426b2cd5d95181293a9baa34102c77c4bbd8
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-11-24 17:00:11 +0300

    GET SET

---
+++ js/get, set.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const persone = {
+    name: "Alex",
+    age: 25,
+
+    get userAge() {               // получаем значение
+        return this.age
+    },
+
+    set userAge(number) {         // устанавливаем значение
+        this.age = number;
+    }
+}
+
+// console.log(persone.userAge)
+// console.log(persone.age)
+
+console.log(persone.userAge = 30)
+console.log(persone.userAge)
