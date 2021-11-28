2b1216923c0d01d31ceed8a301c4a2871bb38976
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-11-28 14:50:53 +0300

    Create modules.js

---
+++ js/modules.js
@@ -0,0 +1,27 @@
+"use strict"
+
+// анонимная самовызывающася функция сразу вызывается и создает локальную область видимости
+
+const number = 1;
+
+(function(){
+   let number = 2;
+   console.log(number);
+   console.log(number + 3)
+}()); 
+
+console.log(number)
+
+// объектный интерфейс
+
+const user = (function(){
+  const private = function () {
+      console.log("Private")
+  }
+
+  return {
+    sayHello: private
+  }
+}());
+
+user.sayHello()
