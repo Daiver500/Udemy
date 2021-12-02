6de315bbe48f4b8a4c34b1f67b715bce85a0ce8c
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-12-02 11:20:35 +0300

    Webpack

--- js/modules.js
+++ js/modules.js
@@ -24,4 +24,4 @@ const user = (function(){
   }
 }());
 
-user.sayHello()
+user.sayHello()                                                                                                                                                         
